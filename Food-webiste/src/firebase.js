6072a6d90bc1c3d4a207bc235617449eb053801d
PanchalJaimin229 <133219925+PanchalJaimin229@users.noreply.github.com>
2024-08-01 12:11:50 +0530

    ..
    ..

---
+++ Food-webiste/src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
